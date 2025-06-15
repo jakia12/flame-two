49bfd600e9815b6c541a67aba5a8af3a9976cfa7
Gazi Jakia Sultanta <107138150+jakia12@users.noreply.github.com>
2025-06-15 09:53:49 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,44 +1,51 @@
-window.addEventListener('load', () => {
-    AOS.init({
-        duration: 700
-    });
+window.addEventListener("load", () => {
+  AOS.init({
+    duration: 700,
+  });
 
-    const code = document.querySelector('.code');
-    const input = document.querySelector('.input');
-    const ca = code.value;
+  const code = document.querySelector(".code");
+  const input = document.querySelector(".input");
+  const ca = code.value;
 
-    code.addEventListener('click', () =>{
-        var button = code;
-        button.select();
-        document.execCommand("copy");
-        input.classList.add('copied');
-        code.classList.add('copied');
-        code.value = 'Copied!';
-        setTimeout(() => {
-            input.classList.remove('copied');
-            code.classList.remove('copied');
-            code.value = ca;
-        }, 2000)
-    })
+  code.addEventListener("click", () => {
+    var button = code;
+    button.select();
+    document.execCommand("copy");
+    input.classList.add("copied");
+    code.classList.add("copied");
+    code.value = "Copied!";
+    setTimeout(() => {
+      input.classList.remove("copied");
+      code.classList.remove("copied");
+      code.value = ca;
+    }, 2000);
+  });
 
-    const burger = document.querySelector('.burger');
-    const mobileMenu = document.querySelector('.mobile-menu');
+  const burger = document.querySelector(".burger");
+  const mobileMenu = document.querySelector(".mobile-menu");
 
-    burger.addEventListener('click', () => {
-        mobileMenu.classList.toggle('active');
-        document.addEventListener('click', closeElement);
-    });
-    
-    function closeElement(event) {
-        if (!mobileMenu.contains(event.target) && !burger.contains(event.target)) {
-            mobileMenu.classList.remove('active');
-            document.removeEventListener('click', closeElement);
-        }
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+    document.addEventListener("click", closeElement);
+  });
+
+  function closeElement(event) {
+    if (!mobileMenu.contains(event.target) && !burger.contains(event.target)) {
+      mobileMenu.classList.remove("active");
+      document.removeEventListener("click", closeElement);
     }
+  }
 
-    document.querySelectorAll('.mobile-menu li').forEach(el => {
-        el.addEventListener('click', () => {
-            mobileMenu.classList.remove('active');
-        })
-    })
-})
+  document.querySelectorAll(".mobile-menu li").forEach((el) => {
+    el.addEventListener("click", () => {
+      mobileMenu.classList.remove("active");
+    });
+  });
+});
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("nav-menu");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
